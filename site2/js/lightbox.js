7968155513fd11952de037854ee7344afa2c6b3c
Michael Mba <mikeysan@users.noreply.github.com>
2023-03-20 08:10:42 +0000

    Update lightbox.js

--- site2/js/lightbox.js
+++ site2/js/lightbox.js
@@ -1,5 +1,59 @@
 /* Lightbox JS */
+// Lightbox functionality
+var $lightbox = $("<div id='lightbox'></div>");
+var $img = $("<img>");
+var $caption = $("<p></p>");
 
+// Add image and caption to lightbox
+$lightbox.append($img);
+$lightbox.append($caption);
+
+// Add lightbox to the document
+$('body').append($lightbox);
+
+// Hide lightbox on click
+$lightbox.click(function() {
+  $lightbox.hide();
+});
+
+// Show the previous image in the lightbox
+function showPrevImage() {
+  var $prev = $('.lightbox-image.active').prev('.lightbox-image');
+  if ($prev.length) {
+    var src = $prev.data('src');
+    var caption = $prev.data('caption');
+    $img.attr('src', src);
+    $caption.text(caption);
+    $('.lightbox-image.active').removeClass('active');
+    $prev.addClass('active');
+  }
+}
+
+// Show the next image in the lightbox
+function showNextImage() {
+  var $next = $('.lightbox-image.active').next('.lightbox-image');
+  if ($next.length) {
+    var src = $next.data('src');
+    var caption = $next.data('caption');
+    $img.attr('src', src);
+    $caption.text(caption);
+    $('.lightbox-image.active').removeClass('active');
+    $next.addClass('active');
+  }
+}
+
+// Add click events to each image in the gallery
+$('.gallery-image').click(function() {
+  var src = $(this).attr('src');
+  var caption = $(this).data('caption');
+  $img.attr('src', src);
+  $caption.text(caption);
+  $lightbox.show();
+  $('.lightbox-image').removeClass('active');
+  $(this).addClass('active');
+});
+
+// Add click events to the prev and next buttons in the light
 document.addEventListener('DOMContentLoaded', function() {
   let images = document.querySelectorAll('.lightbox-image');
   images.forEach(function(image) {
