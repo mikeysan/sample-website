193c53a9d611ff40ec9521a941184028b1f8ae6c
Michael Mba <mikeysan@users.noreply.github.com>
2023-03-20 07:28:18 +0000

    Create models.py

---
+++ site2/models.py
@@ -0,0 +1,11 @@
+from app import db
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    message = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Contact {self.name}>'
