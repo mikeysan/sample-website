fb3f3c2e8db68f498f08f2c93083d1568ef92b8a
Michael Mba <mikeysan@users.noreply.github.com>
2023-03-20 07:50:26 +0000

    Update app.py

--- site2/app.py
+++ site2/app.py
@@ -1,72 +1,46 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
-from flask_wtf.csrf import CSRFProtect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_mail import Mail, Message
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-import os
+from forms import ContactForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'supersecretkey'  # change this to a secret key of your own
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///portfolio.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'secretkey'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
-app.config['MAIL_DEFAULT_SENDER'] = 'your_email_here@gmail.com'  # change this to your email address
+app.config['MAIL_USERNAME'] = 'youremail@gmail.com'
+app.config['MAIL_PASSWORD'] = 'yourpassword'
 
-db = SQLAlchemy(app)
-csrf = CSRFProtect(app)
 mail = Mail(app)
-
-
-class Contact(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(50), nullable=False)
-    message = db.Column(db.String(500), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return f'<Contact {self.name}>'
-
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-
 @app.route('/gallery')
 def gallery():
     return render_template('gallery.html')
 
-
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        message = request.form['message']
-        contact = Contact(name=name, email=email, message=message)
-        db.session.add(contact)
-        db.session.commit()
-
-        msg = Message(subject='New message from your photography website',
-                      recipients=[app.config['MAIL_DEFAULT_SENDER']])
-        msg.body = f"You have a new message from {name} ({email}) on your photography website.\n\nMessage: {message}"
+    form = ContactForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        subject = form.subject.data
+        message = form.message.data
+        msg = Message(subject, sender=email, recipients=['youremail@gmail.com'])
+        msg.body = f"From: {name}\nEmail: {email}\n\n{message}"
         mail.send(msg)
+        return redirect(url_for('contact_success'))
+    return render_template('contact.html', form=form)
 
-        flash('Your message has been sent!', 'success')
-        return redirect(url_for('contact'))
-
-    return render_template('contact.html')
-
+@app.route('/contact/success')
+def contact_success():
+    return render_template('contact_success.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
